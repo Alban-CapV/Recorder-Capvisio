bee661fb355ad8e7e18b570656e1f41313b9838a
Alban TIPREZ <alban.tiprez@cap-visio.com>
2021-03-18 16:57:35 +0100

    New version Project

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-# #!/usr/bin/env sh
-
-# export RECORD_STORAGE='$'
-# envsubst '${RECORD_STORAGE}' < /etc/nginx/nginx.conf
-
-docker run -t -d \
--v /var/Recoder-Capvisio:/app \
--p 1935:1935 -p 1936:1936 \
---mount type=tmpfs,destination=/tmp
